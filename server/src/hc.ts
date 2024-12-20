0dfc7b39831b968b58bb655c000bbdea3553776b
growwidtalha <growwithtalha2@gmail.com>
2024-12-20 19:26:50 +0500

    feat: Added authentication

--- server/src/hc.ts
+++ server/src/hc.ts
@@ -2,7 +2,9 @@ import app  from './app'
 import { hc } from 'hono/client'
 
 // assign the client to a variable to calculate the type when compiling
-const client = hc<typeof app>('')
+const client = hc<typeof app>(app)
 export type Client = typeof client
+
+
 
 export const hcWithType = (...args: Parameters<typeof hc>): Client => hc<typeof app>(...args)
