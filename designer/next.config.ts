82058f01948ec4d6dcd8dbafb06d8ef5326b49bd
Talha Ali <growwithtalha2@gmail.com>
2025-07-05 22:36:44 +0500

    hehe

--- designer/next.config.ts
+++ designer/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/designer",
+  assetPrefix: "/designer",
   images: {
     remotePatterns: [
       {
--- printing_provider/next.config.ts
+++ printing_provider/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/printing-provider",
+  assetPrefix: "/printing-provider",
   images: {
     remotePatterns: [
       {
